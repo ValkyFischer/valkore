f21c88823110e510f0ea0873e2e2c0a32f62149d
ValkyFischer <fischer@valkyteq.com>
2022-10-16 13:21:23 +0200

    0.0.2: - Show contributors in module loading log - Allow passing empty thread message - Move interval to Settings in config

--- valkframe.py
+++ valkframe.py
@@ -21,7 +21,7 @@ class ValkFrame:
 
 	def __init__(self):
 		self._timer = scheduler(time, sleep)
-		self._timerInterval = int(CONFIG['Base']['interval'])  # config [sec]
+		self._timerInterval = int(CONFIG['Settings']['interval'])  # config [sec]
 		self._timerPrio = 1
 
 
@@ -33,7 +33,11 @@ class ValkFrame:
 			files = next(os.walk(f"modules/{module}"))[2]
 			if "config.ini" in files and f"{module}.py" in files:
 				modCfg = Config(path=f"modules/{module}/config.ini").readConfig()
-				LOGGER.Info(f"Loading module: {modCfg['Base']['name']} (v{modCfg['Base']['version']} | {modCfg['Base']['author']})")
+				appName = modCfg['VFrame']['name']
+				appVersion = modCfg['VFrame']['version']
+				appAuthor = f"Author: {modCfg['VFrame']['author']}"
+				appEditor = f"{appAuthor} | Editor: {modCfg['VFrame']['modify']}" if "modify" in modCfg['VFrame'] else appAuthor
+				LOGGER.Info(f"Loading module: {appName} | v{appVersion} | {appEditor}")
 				load[module] = modCfg
 			else:
 				LOGGER.Warn(f"Missing dependency: {module}")
@@ -62,7 +66,7 @@ class ValkFrame:
 		try:
 			line = que.get_nowait()
 		except Empty:
-			LOGGER.Info(f"Started module: {cfg['Base']['name']}")
+			pass
 		else:  # got line
 			LOGGER.Debug(f"{modlue} Output: {line}")
 
@@ -78,21 +82,21 @@ class ValkFrame:
 		- Starts CONFIG Observer
 		"""
 		# Write log
-		LOGGER.Info(f"*"*69)
-		LOGGER.Info(f"PROJECT     : {CONFIG['Base']['name']} | ..a project by VALKYTEQ")
-		LOGGER.Info(f"DESCRIPTION : {CONFIG['Base']['description']}")
-		LOGGER.Info(f"AUTHOR      : {CONFIG['Base']['author']}")
-		LOGGER.Info(f"VERSION     : {CONFIG['Base']['version']}")
-		LOGGER.Info(f"*"*69)
+		LOGGER.Info(f"*" * 69)
+		LOGGER.Info(f"PROJECT     : {CONFIG['VFrame']['name']} | ..a project by VALKYTEQ")
+		LOGGER.Info(f"DESCRIPTION : {CONFIG['VFrame']['description']}")
+		LOGGER.Info(f"AUTHOR      : {CONFIG['VFrame']['author']}")
+		LOGGER.Info(f"VERSION     : {CONFIG['VFrame']['version']}")
+		LOGGER.Info(f"*" * 69)
 
 		# load modules
-		modules = vf.getModules()
+		modules = self.getModules()
 		if len(modules) > 0:
 			for module, cfg in modules.items():
-				vf.startModule(module, cfg)
+				self.startModule(module, cfg)
 		else:
 			LOGGER.Info(f"No modules loaded")
-		# LOGGER.Info(f"-" * 69)
+		LOGGER.Info(f"*" * 69)
 
 		# Start and enter Interval Timer
 		self._timer.enter(1, self._timerPrio, self.controller, (self._timer,))
